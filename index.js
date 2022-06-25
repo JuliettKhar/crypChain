6ea386dc5bb8cddcd9bbe1803030aab9a398e969
julia kharlamova <juliettkhar@gmail.com>
2022-06-25 16:47:34 +0300

    grab valid transactions

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ const app = express()
 const blockchain = new Blockchain();
 const transactionPool = new TransactionPool()
 const wallet = new Wallet()
-const pubsub = new PubSub({ blockchain, transactionPool })
+const pubsub = new PubSub({blockchain, transactionPool})
 
-const  DEFAULT_PORT = 3000;
+const DEFAULT_PORT = 3000;
 const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`
 
 app.use(bodyParser.json());
@@ -21,38 +21,38 @@ app.get('/api/blocks', (req, res) => {
     res.json(blockchain.chain)
 })
 app.post('/api/mine', (req, res) => {
-    const { data } = req.body;
+    const {data} = req.body;
 
-    blockchain.addBlock({ data })
+    blockchain.addBlock({data})
     pubsub.broadcastChain();
     res.redirect('/api/blocks')
 })
 
 app.post('/api/transact', ((req, res) => {
-    const { amount, recipient } = req.body
-    let transaction = transactionPool.existingTransaction({ inputAddress: wallet.publicKey})
+    const {amount, recipient} = req.body
+    let transaction = transactionPool.existingTransaction({inputAddress: wallet.publicKey})
 
     try {
         if (transaction) {
-            transaction.update({ senderWallet: wallet, recipient, amount })
+            transaction.update({senderWallet: wallet, recipient, amount})
         } else {
             transaction = wallet.createTransaction(({recipient, amount}))
         }
-        transactionPool.setTransaction(transaction)
-        pubsub.broadcastTransaction(transaction)
-
-        res.json({type: 'success', transaction})
     } catch (e) {
-        res.status(400).json({type: 'error', message: e.message})
+        return res.status(400).json({type: 'error', message: e.message})
     }
+
+    transactionPool.setTransaction(transaction)
+    pubsub.broadcastTransaction(transaction)
+    res.json({type: 'success', transaction})
 }))
 
 app.get('/api/transaction-pool-map', (req, res) => {
-res.json(transactionPool.transactionMap)
+    res.json(transactionPool.transactionMap)
 })
 
 const syncWithRootState = () => {
-    request({ url: `${ROOT_NODE_ADDRESS}/api/blocks`}, (err, resp, body) => {
+    request({url: `${ROOT_NODE_ADDRESS}/api/blocks`}, (err, resp, body) => {
         if (!err && resp.statusCode === 200) {
             const rootChain = JSON.parse(body)
 
@@ -61,9 +61,9 @@ const syncWithRootState = () => {
         }
     })
 
-    request({ url: `${ROOT_NODE_ADDRESS}/api/transaction-pool-map`}, (err, resp, body) => {
+    request({url: `${ROOT_NODE_ADDRESS}/api/transaction-pool-map`}, (err, resp, body) => {
         if (!err && resp.statusCode === 200) {
-            const rootTransactionPoolMap  = JSON.parse(body)
+            const rootTransactionPoolMap = JSON.parse(body)
 
             console.log('replace transaction pool map on a sync with', rootTransactionPoolMap)
             transactionPool.setMap(rootTransactionPoolMap)
--- wallet/transaction-pool.test.js
+++ wallet/transaction-pool.test.js
@@ -3,22 +3,68 @@ const Transaction = require('./transaction')
 const Wallet = require('./index')
 
 describe('TransactionPool', () => {
-    let transactionPool, transaction;
+    let transactionPool, transaction, senderWallet;
 
     beforeEach(() => {
         transactionPool = new TransactionPool()
+        senderWallet = new Wallet()
+
         transaction = new Transaction({
-            senderWallet: new Wallet(),
+            senderWallet,
             recipient: 'fake-recipient',
             amount: 50
         })
     })
 
     describe('setTransaction()', () => {
-        it('should adds a transaction', function () {
+        it('should adds a transaction', () => {
             transactionPool.setTransaction(transaction)
 
             expect(transactionPool.transactionMap[transaction.id]).toBe(transaction)
+        });
+    })
+
+    describe('existingTransaction()', () => {
+        it('should returns an existing transaction given an input address', () => {
+            transactionPool.setTransaction(transaction)
+            expect(transactionPool.existingTransaction({ inputAddress: senderWallet.publicKey})).toBe(transaction)
+        });
+    })
+
+    describe('validTransactions()', () => {
+        let validTransactions, errorMock;
+
+        beforeEach(() => {
+            validTransactions = [];
+            errorMock = jest.fn();
+            global.console.error = errorMock
+
+            for (let i = 0; i < 10; i++) {
+                transaction = new Transaction({
+                    senderWallet,
+                    recipient: 'any-recipient',
+                    amount: 30
+                });
+
+                if (i%3 ===0) {
+                    transaction.input.amount = 9999999
+                } else if(i%3===1) {
+                    transaction.input.signature = new Wallet().sign('foo')
+                } else {
+                   validTransactions.push(transaction)
+                }
+
+                transactionPool.setTransaction(transaction)
+            }
+        })
+
+        it('should returns a valid transaction', () => {
+            expect(transactionPool.validTransactions()).toEqual(validTransactions)
+        });
+
+        it('should logs errors for invalid transactions', () => {
+            transactionPool.validTransactions();
+            expect(errorMock).toHaveBeenCalled();
         });
     })
 })
